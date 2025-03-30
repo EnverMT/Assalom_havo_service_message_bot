4389477e3acc40432cdb4157f4626147b94407fb
EnverMT <anvarmtg@gmail.com>
2025-03-30 12:40:34 +0500

    Update handler to log user kicks and joins with improved status handling

--- handler.py
+++ handler.py
@@ -1,6 +1,6 @@
 from aiogram import F, Router
 from config import load_config
-from aiogram.filters import ChatMemberUpdatedFilter, IS_MEMBER, IS_NOT_MEMBER
+from aiogram.filters import ChatMemberUpdatedFilter, IS_MEMBER, IS_NOT_MEMBER, KICKED, LEFT
 from aiogram.types import ChatMemberUpdated, Message
 from logger import logger
 
@@ -16,15 +16,17 @@ async def delete_service_message(message):
         logger.error(f"Error deleting service message: {e}")
 
 
-@router.chat_member(ChatMemberUpdatedFilter(member_status_changed=IS_NOT_MEMBER >> IS_MEMBER))
-async def log_approved_join_request(event: ChatMemberUpdated):
-    approver = event.from_user
+@router.chat_member(ChatMemberUpdatedFilter(member_status_changed=IS_MEMBER >> KICKED))
+async def log_user_kicked(event: ChatMemberUpdated):
+
+    kicker = event.from_user
     new_member = event.new_chat_member.user
 
-    approver_url = f"<a href='tg://user?id={approver.id}'>{approver.first_name}</a>"
+    kicker_url = f"<a href='tg://user?id={kicker.id}'>{kicker.first_name}</a>"
     new_member_url = f"<a href='tg://user?id={new_member.id}'>{new_member.first_name}</a>"
 
-    text = f"User joined: {new_member_url} (ID:<code>{new_member.id}</code>)\n" f"Approved by: {approver_url}\n"
+    text = f"User kicked: {new_member_url} (ID:<code>{new_member.id}</code>)\n"
+    text += f"Banned by: {kicker_url} (ID:<code>{kicker.id}</code>)\n"
 
     await event.bot.send_message(
         chat_id=config.log_group.id,
@@ -35,12 +37,31 @@ async def log_approved_join_request(event: ChatMemberUpdated):
     )
 
 
-@router.chat_member(ChatMemberUpdatedFilter(member_status_changed=IS_MEMBER >> IS_NOT_MEMBER))
+@router.chat_member(ChatMemberUpdatedFilter(member_status_changed=IS_MEMBER >> LEFT))
 async def log_user_left(event: ChatMemberUpdated):
     new_member = event.new_chat_member.user
     new_member_url = f"<a href='tg://user?id={new_member.id}'>{new_member.first_name}</a>"
 
     text = f"User left: {new_member_url} (ID:<code>{new_member.id}</code>)\n"
+
+    await event.bot.send_message(
+        chat_id=config.log_group.id,
+        message_thread_id=config.log_group.thread_id,
+        text=text,
+        parse_mode="HTML",
+        disable_web_page_preview=True,
+    )
+
+
+@router.chat_member(ChatMemberUpdatedFilter(member_status_changed=IS_NOT_MEMBER >> IS_MEMBER))
+async def log_approved_join_request(event: ChatMemberUpdated):
+    approver = event.from_user
+    new_member = event.new_chat_member.user
+
+    approver_url = f"<a href='tg://user?id={approver.id}'>{approver.first_name}</a>"
+    new_member_url = f"<a href='tg://user?id={new_member.id}'>{new_member.first_name}</a>"
+
+    text = f"User joined: {new_member_url} (ID:<code>{new_member.id}</code>)\n" f"Approved by: {approver_url}\n"
 
     await event.bot.send_message(
         chat_id=config.log_group.id,
